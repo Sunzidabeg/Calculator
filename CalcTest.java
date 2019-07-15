d405b3f75cff07871557f21e619dc245bb59a957
Sunzidabeg <50387582+Sunzidabeg@users.noreply.github.com>
2019-07-14 23:04:34 -0500

    Add files via upload

---
+++ CalcTest.java
@@ -0,0 +1,10 @@
+
+package Calculator;
+
+
+public class CalcTest {
+        public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.getResults(10.5, '+', 5.2));
+    }
+}
